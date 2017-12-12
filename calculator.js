0d420edcbfc2f57fd2899d9703fd50cdf275ad0d
Stephanie Shea <stephcshea@gmail.com>
2017-12-12 18:24:59 +0000

    first commit

---
+++ calculator.js
@@ -0,0 +1,28 @@
+/* global $*/
+$(document).ready(function(){
+   
+
+// var one = 1;
+// var two = 2;
+// var three = 3;
+// var four = 4;
+// var five = 5;
+// var six = 6;
+// var seven = 7;
+// var eight = 8;
+// var nine = 9;
+// var plus;
+// var minus;
+// var mult;
+// var div;
+// var display;
+// var clear;
+// var clearAll;
+
+
+$( "button.one" ).on( "click", function() {
+        console.log(1);
+    });
+        
+        
+    });
