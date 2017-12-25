ba5f1b67acc5e35653f99b898f5914d401a80201
Stephanie Shea <stephcshea@gmail.com>
2017-12-25 12:59:10 +0000

    working out small pieces

--- calculator.js
+++ calculator.js
@@ -1,25 +1,34 @@
 /* global $*/
 $(document).ready(function(){
-   
 
-// var one = 1;
-// var two = 2;
-// var three = 3;
-// var four = 4;
-// var five = 5;
-// var six = 6;
-// var seven = 7;
-// var eight = 8;
-// var nine = 9;
-// var plus;
-// var minus;
-// var mult;
-// var div;
-// var display;
-// var clear;
-// var clearAll;
-var display = [];
-
+var number;
+var display = "";
+var total= $("#total");
+    total.text("0");
+// function getTotal() { 
+        
+//         var total = display.reduce(function(a,b){return a+b});
+//   $('#total').html(total);
+// }
+    //  $('#total').html(total);
+    
+    $( "button.equal" ).on( "click", function () {
+    
+    for (var i = 0; i <display.length; i++)
+    
+    {
+    total += parseInt(display[i]);
+    console.log(display);
+    total = document.getElementById('total').innerHTML;
+    return total;
+    }
+    
+    // total = document.getElementById('total').innerHTML;
+    // return total;
+    
+    });
+    
+    
 $( "button.zero" ).on( "click", function() {
         console.log(0);
         // addKey(key) {
@@ -29,35 +38,42 @@ $( "button.zero" ).on( "click", function() {
     });
     
 $( "button.one" ).on( "click", function() {
-        console.log(1);
+        // display.push(1);
+        display ++;
+    document.getElementById("displayHere").innerHTML += 1;
+    
     });
     
 $( "button.two" ).on( "click", function() {
-        console.log(2);
+        // display.push(2);
+        display ++;
+         document.getElementById("displayHere").innerHTML += 2;
     });
-$( "button.three" ).on( "click", function() {
-        console.log(3);
-    }); 
-$( "button.four" ).on( "click", function() {
-        console.log(4);
-    }); 
-$( "button.five" ).on( "click", function() {
-        console.log(5);
-    }); 
-$( "button.six" ).on( "click", function() {
-        console.log(6);
-    }); 
-$( "button.seven" ).on( "click", function() {
-        console.log(7);
-    }); 
-$( "button.eight" ).on( "click", function() {
-        console.log(8);
-    }); 
-$( "button.nine" ).on( "click", function() {
-        console.log(9);
-    }); 
+// $( "button.three" ).on( "click", function() {
+//         display.push(3);
+//     }); 
+// $( "button.four" ).on( "click", function() {
+//         display.push(4);
+//     }); 
+// $( "button.five" ).on( "click", function() {
+//         display.push(5);
+//     }); 
+// $( "button.six" ).on( "click", function() {
+//         display.push(6);
+//     }); 
+// $( "button.seven" ).on( "click", function() {
+//         console.log(7);
+//     }); 
+// $( "button.eight" ).on( "click", function() {
+//         console.log(8);
+//     }); 
+// $( "button.nine" ).on( "click", function() {
+//         console.log(9);
+//     }); 
 $( "button.add" ).on( "click", function() {
-        console.log('+');
+display ++;
+        document.getElementById("displayHere").innerHTML += ("+");
+
     }); 
 $( "button.subtract" ).on( "click", function() {
         console.log('-');
@@ -67,9 +83,48 @@ $( "button.multiply" ).on( "click", function() {
     }); 
 $( "button.divide" ).on( "click", function() {
         console.log('/');
-    });     
-$( "button.equal" ).on( "click", function() {
-        console.log('=');
-    });     
-        
+    });
+$( "button.decimal" ).on( "click", function() {
+        console.log('.');
     });    
+    
+$( "button.equal" ).on( "click", function () {
+//     return display; {
+//     total = display.reduce(function(prev,next,index) {
+//   console.log(index + ":" + prev + ":" + next);
+// prev += next;
+// return prev;
+// },0);
+
+});
+    
+    
+
+
+// function getSum(total, num) {
+//     return total + num;
+// }
+// function myFunction(item) {
+//     document.getElementById("demo").innerHTML = display.reduce(getSum);
+// }
+
+
+});
+
+   
+        
+
+// function getSum(total, num) {
+//     return total + num;
+         
+    
+//     console.log(display);
+//     return display;
+
+//          Zvar total = display.reduce( function(total, amount) {
+//   return total + amount; 
+//     });    
+    
+//     const reducer = (accumulator, currentValue) => accumulator + currentValue;
+//         console.log(display.reduce(reducer));
+//             return accumulator + currentValue;
